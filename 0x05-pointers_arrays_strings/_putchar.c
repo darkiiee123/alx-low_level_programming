0c4d921b72af44d94efaf96dd04684aab5dfa7c3
Abdulhameed <abdulhameedabdulrazaq123@gmail.com>
2022-10-24 19:58:23 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+  * _putchar - writes the character c to stdout
+  * @c: The character to print
+  * Return: always 0 (success)
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
