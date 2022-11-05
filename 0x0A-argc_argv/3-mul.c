2bf1e71233aa3d224a4b94a85ccbc12ad93768cd
Abdulhameed <abdulhameedabdulrazaq123@gmail.com>
2022-11-05 20:52:20 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
