a5c502cbdc034f9e70e97768088c5cb694e7f7c0
Abdulhameed <abdulhameedabdulrazaq123@gmail.com>
2022-11-05 20:00:18 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always 0 (success)
+  */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
