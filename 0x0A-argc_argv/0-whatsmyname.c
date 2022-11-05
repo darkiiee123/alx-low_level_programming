539a1499e0a3e79762e3fe93a7ec4045717df551
Abdulhameed <abdulhameedabdulrazaq123@gmail.com>
2022-11-05 19:45:02 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 0 if successful
+  */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
